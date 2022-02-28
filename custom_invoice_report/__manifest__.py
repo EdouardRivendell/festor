b0020139134c51cf95d14c5ed106fe7a3d782cdb
bhanu <davuluri@sociusus.com>
2022-02-28 10:08:03 +0530

    Custom Invoice report

---
+++ custom_invoice_report/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Custom Invoice Report",
+    'category': "Hidden",
+    'summary': 'Customisation of invoice report',
+
+    'description': """
+        Customisation of invoice report
+    """,
+
+    'depends': ['account'],
+
+    'data': [
+            'views/page_header_visibility.xml',
+            'report/invoice_report.xml',
+            'report/invoice_report_template.xml'
+    ],
+    'installable': True,
+    'qweb': [
+
+    ],
+}
